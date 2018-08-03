811158f0eea0b71523974c73c178755e74a0d7ea
Edwin Smith <edwin@showit.co>
2018-08-02 19:53:59 -0700

    change up things for speed

--- web/themes/custom/pneumatic/js/foundation_init.js
+++ web/themes/custom/pneumatic/js/foundation_init.js
@@ -14,4 +14,37 @@
         }
     };
 
+    /**
+     * Adds the "active" class to top bar <li> elements with active child links.
+     */
+    Drupal.behaviors.foundationTopBarActive = {
+        attach: function (context, settings) {
+            var $active_links = $(context).find('.top-bar .menu-item > a.is-active');
+            if ($active_links.length) {
+                $active_links.once('foundationTopBarActive').each(function() {
+                    $(this).parent().addClass('active');
+                });
+            }
+        }
+    };
+
+    /**
+     * Displays status messages in a Foundation reveal modal.
+     */
+    Drupal.behaviors.foundationStatusInReveal = {
+        attach: function (context, settings) {
+            $('#status-messages').once('foundation-reveal').each(function() {
+                // Move the status messages out of the highlighted region.
+                var $messages = $(this);
+                // var $region = $messages.parent();
+                // $messages.appendTo('body');
+                $messages.foundation('open');
+
+                // This is required as this region will likely be empty after status
+                // messages are removed.
+                // $region.html($region.html().replace(/\n/g, ''));
+            });
+        }
+    };
+
 })(jQuery, Drupal);
