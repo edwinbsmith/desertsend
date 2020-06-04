01d09a868b99f44324ee379af5462b82189552f7
Edwin Smith <edwin@showit.co>
2020-06-03 19:23:00 -0700

    updated foundation

--- web/themes/custom/pneumatic/gulpfile.js
+++ web/themes/custom/pneumatic/gulpfile.js
@@ -1,36 +1,45 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var autoprefixer = require('autoprefixer');
 
 var sassPaths = [
-    'node_modules/foundation-sites/scss',
-    'node_modules/motion-ui/src',
-    'node_modules/mediaelement/build/mediaelementplayer.css'
+  'node_modules/foundation-sites/scss',
+  'node_modules/motion-ui/src',
+  'node_modules/mediaelement/build/mediaelementplayer.css'
 ];
 
-gulp.task('sass', function () {
-    return gulp.src('scss/app.scss')
-        .pipe($.sass({
-            includePaths: sassPaths,
-            outputStyle: 'compressed' // if css compressed **file size**
-        })
-            .on('error', $.sass.logError))
-        .pipe($.autoprefixer({
-            browsers: ['last 2 versions', 'ie >= 9']
-        }))
-        .pipe(gulp.dest('css'));
-});
+var cssFiles = [
+  './node_modules/foundation-sites/dist/css/*.css',
+  './node_modules/motion-ui/dist/*.css'
+];
+
+var jsFiles = [
+  './node_modules/foundation-sites/dist/js/*.js',
+  './node_modules/motion-ui/dist/*.js'
+];
+
+function sass() {
+  return gulp.src('scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths,
+      outputStyle: 'compressed' // if css compressed **file size**
+    })
+      .on('error', $.sass.logError))
+    .pipe($.postcss([
+      autoprefixer({browsers: ['last 2 versions', 'ie >= 9']})
+    ]))
+    .pipe(gulp.dest('css'));
+}
+
+function serve() {
+  gulp.watch('scss/**/*.scss', sass);
+}
 
 gulp.task('copy', function () {
-    gulp.src('node_modules/foundation-sites/dist/css/*.css')
-        .pipe($.copy('css', {prefix: 4}));
-    gulp.src('node_modules/foundation-sites/dist/js/*.js')
-        .pipe($.copy('js', {prefix: 4}));
-    gulp.src('node_modules/motion-ui/dist/*.css')
-        .pipe($.copy('css', {prefix: 3}));
-    gulp.src('node_modules/motion-ui/dist/*.js')
-        .pipe($.copy('js', {prefix: 3}));
+  gulp.src(jsFiles).pipe(gulp.dest('./js/'));
+  gulp.src(cssFiles).pipe(gulp.dest('./css/'));
 });
 
-gulp.task('default', ['sass', 'copy'], function () {
-    gulp.watch(['scss/**/*.scss'], ['sass']);
-});
+gulp.task('sass', sass);
+
+gulp.task('default', gulp.series('sass', serve));
